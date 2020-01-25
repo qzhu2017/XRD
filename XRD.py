663e1c64f3bd88f37c1818f5079643878ad21eab
Dean <sayred1@unlv.nevada.edu>
2020-01-25 11:14:11 -0800

    constant fwhm

--- XRD.py
+++ XRD.py
@@ -739,13 +739,16 @@ class XRD(object):
             theta2 (1D array): simulated theta values 
             xrd_intensity (1D array): simulated peaks 
             N (int): Resolution for profiling arrays 
-            Keyword Arguments (dict):
-                Key 1 (string): Function used to profile spectra (must be gaussian, lorentzian, or split-type). 
-                    If gaussian:
+            Keyword Arguments (dict): The user can choose between theta dependent OR independent fwhm.
+                Key 1 (string): Function used to profile spectra (must be gaussian, lorentzian, or split-type).
+                Theta independent:
+                    Key 2 (float): constant value for fwhm
+                Theta dependent:
+                    Key 1 = gaussian:
                         Key 2 (float/int): adjustable parameter (V) for theta dep. fwhm
-                    If lorentzian:
+                    Key 1 = lorentzian:
                         key 2 (float/int): adjustable parameter (X) for theta dep. fwhm
-                    If split-type:
+                    Key 1 = split-type:
                         Key 2-4 (float/int): adjustable parameters (U, V, W) for theta dep. fwhm
                         Key 5 (float/int): assymetry parameter (A)
                         key 6 (float/int): mixing parameter (eta_l)
@@ -762,22 +765,32 @@ class XRD(object):
         
         for i,j in zip(range(len(theta2)),range(len(xrd_intensity))):
             if profile == 'gaussian':
-                V = kwargs['V']
-                fwhm = V*np.tan(np.pi*theta/2/180)
-                tmp = self.gaussian_profile(peak,theta,g2thetas)
+                try:
+                    V = kwargs['V']
+                    fwhm = V*np.tan(np.pi*theta/2/180)
+                except:
+                    fwhm = kwargs['FWHM']
+                tmp = self.gaussian_profile(peak,theta,g2thetas,fwhm)
+
             elif profile == 'lorentzian':
-                X = kwargs['X']
-                fwhm = X/np.cos(np.pi*theta/2/180)
+                try:
+                    X = kwargs['X']
+                    fwhm = X/np.cos(np.pi*theta/2/180)
+                except:
+                    fwhm = kwargs['FWHM']
                 tmp = self.lorentzian_profile(peak,theta,g2thetas)
+            
             elif profile == 'split-type': 
-                U = kwargs['U']
-                V = kwargs['V']
-                W = kwargs['W']
+                try:
+                    U = kwargs['U']
+                    V = kwargs['V']
+                    W = kwargs['W']
+                    fwhm = np.sqrt(U*np.tan(np.pi*theta2[i]/2/180)**2 + V*np.tan(np.pi*theta2[i]/2/180) + W)
+                except:
+                    fwhm = kwargs['FWHM']
 
-                fwhm = np.sqrt(U*np.tan(np.pi*theta2[i]/2/180)**2 + V*np.tan(np.pi*theta2[i]/2/180) + W)
-                x = g2thetas - theta2[i]
-
-                tmp = self.split_type(x, xrd_intensity[i], fwhm, N, **kwargs) 
+                x = g2thetas - theta2[i]    
+                tmp = self.split_type(x, xrd_intensity[i], N, fwhm, **kwargs) 
 
             else:
                 msg = profile + ' is not supported'
@@ -795,26 +808,25 @@ class XRD(object):
         tmp = 1 + 4*((alpha - theta2)/fwhm)**2
         return I0 * 1/tmp
 
-    def split_type(self, x, I0, fwhm, N, **kwargs):
+    def split_type(self, x, I0, N, fwhm, **kwargs):
         tmp = np.zeros((N))
         for k, dx in enumerate(x):
             if dx < 0:
                 A = kwargs['A']
                 eta_l = kwargs['eta_l']
                 eta_h = kwargs['eta_h']
-            
+
             else:
                 A = 1/kwargs['A'] 
                 eta_l = kwargs['eta_h']
                 eta_h = kwargs['eta_l']
 
-    
             tmp[k] = ((1+A)*(eta_h + np.sqrt(np.pi*np.log(2))*(1-eta_h))) /\
                      (eta_l + np.sqrt(np.pi*np.log(2)) * (1-eta_l) + A*(eta_h + \
                      np.sqrt(np.pi*np.log(2))*(1-eta_h))) * (eta_l*2/(np.pi*fwhm) * \
                      (1+((1+A)/A)**2 * (dx/fwhm)**2)**(-1) + (1-eta_l)*np.sqrt(np.log(2)/np.pi)* \
                      2/fwhm *np.exp(-np.log(2) * ((1+A)/A)**2 * (dx/fwhm)**2))
-        
+
         return I0 * tmp
 
     def pxrdf(self):
