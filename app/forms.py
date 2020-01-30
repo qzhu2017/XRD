783a923fb9ade8e4478c096b45b2f7f1b230cb0f
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2020-01-29 17:24:29 -0800

    Added secure CIF upload w/ ext./size validation

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import TextAreaField, StringField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class CalcForm(FlaskForm):
-    cif = TextAreaField('CIF', validators=[DataRequired()])
+    cif = FileField('CIF', validators=[FileRequired(), FileAllowed(['cif'], '.CIF (Crystallographic Information Files) only!')])
     wavelength = StringField('Wavelength (&#8491;)', validators=[DataRequired()])
     theta = StringField('Max. 2&theta; value', validators=[DataRequired()])
     submit = SubmitField('Calculate')
     
+# Create separate classes for each profiling branch w/ member parameters
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,19 @@
+import os
 from flask import render_template, flash
 from app import app
 from app.forms import CalcForm
+from werkzeug.utils import secure_filename
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = CalcForm()
     if form.validate_on_submit():
-        flash('XRD Calculation for CIF, Wavelength={}Å, 2θ={}°'.format(form.wavelength.data, form.theta.data))
+        f = form.cif.data
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(
+            app.instance_path, 'CIF', filename
+        ))
+        flash('XRD Calculation for {}, Wavelength={}Å, 2θ={}°'.format(f.filename, form.wavelength.data, form.theta.data))
         return render_template('index.html', title='Calculator', form=form)
     return render_template('index.html', title='Calculator', form=form)
