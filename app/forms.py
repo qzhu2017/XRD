347370fa51cec7fdfc3060b904b2cf7991b7f09e
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2020-01-30 19:52:48 -0800

    Added plotly graph w/ add'l validators

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,32 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import TextAreaField, StringField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import FloatField, SubmitField
+from wtforms.validators import DataRequired, NumberRange
 
 class CalcForm(FlaskForm):
-    cif = FileField('CIF', validators=[FileRequired(), FileAllowed(['cif'], '.CIF (Crystallographic Information Files) only!')])
-    wavelength = StringField('Wavelength (&#8491;)', validators=[DataRequired()])
-    theta = StringField('Max. 2&theta; value', validators=[DataRequired()])
+    cif = FileField(
+        label='CIF',
+        validators=[FileRequired(), FileAllowed(
+            ['cif'],
+            message='.CIF (Crystallographic Information Files) only!')],
+        description='Upload a Crystallographic Information File (.CIF)')
+    wavelength = FloatField(
+        label='&lambda; (&#8491;)',
+        validators=[DataRequired(), NumberRange(
+            min=0.1,
+            max=50,
+            message='Must be between %(min)s Å and %(max)s Å'
+        )],
+        description='Wavelength in angstroms',
+        default=1.54056)
+    theta = FloatField(
+        label='2&theta;<sub>max</sub> (&deg;)',
+        validators=[DataRequired(), NumberRange(
+            min=28,
+            max=180,
+            message='Must be between %(min)s° and %(max)s°'
+        )],
+        description='Maximum diffraction angle in degrees',default=90)
     submit = SubmitField('Calculate')
     
-# Create separate classes for each profiling branch w/ member parameters
+# Create separate classes for each profiling branch w/ member parameters (see "Field Enclosures")
--- app/routes.py
+++ app/routes.py
@@ -3,17 +3,31 @@ from flask import render_template, flash
 from app import app
 from app.forms import CalcForm
 from werkzeug.utils import secure_filename
+from XRD import crystal, XRD
+from .plotly_pxrd import plot_pxrd
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = CalcForm()
     if form.validate_on_submit():
+        # Save uploaded CIF to disk
         f = form.cif.data
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(
-            app.instance_path, 'CIF', filename
-        ))
-        flash('XRD Calculation for {}, Wavelength={}Å, 2θ={}°'.format(f.filename, form.wavelength.data, form.theta.data))
-        return render_template('index.html', title='Calculator', form=form)
+        save_path = os.path.join(app.instance_path, 'CIF', secure_filename(f.filename))
+        f.save(save_path)
+        
+        # Retrieve form data
+        wavelength = form.wavelength.data
+        max2theta = form.theta.data
+        N = 10000
+        # Alert w/ submission data
+        flash('XRD calculated for {}, λ={} Å, 2θ={}°'.format(f.filename, wavelength, max2theta))
+
+        # Calculate and plot
+        struct = crystal('cif', filename=save_path)
+        xrd1 = XRD(struct, wavelength, max2theta) 
+        xrd1.get_profile(xrd1.theta2, xrd1.xrd_intensity, N)
+        plot = plot_pxrd(xrd1)
+
+        return render_template('index.html', title='Calculator', form=form, plot=plot)
     return render_template('index.html', title='Calculator', form=form)
