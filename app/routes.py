305ad99f0ee75d20e86184d2e4edd0fce367e2ae
Qiang Zhu <alecfans@gmail.com>
2020-02-04 19:55:49 -0800

    now call flask app from the installed pxrd package

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ from flask import render_template, flash
 from app import app
 from app.forms import CalcForm
 from werkzeug.utils import secure_filename
-from XRD import crystal, XRD
-from .plotly_pxrd import plot_pxrd
+from pyxtal_xrd.XRD import XRD
+from ase.io import read
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -33,10 +33,10 @@ def index():
         eta_l = 0.611844  # Mixing parameter, eta_L0
         profile = {'function':'split-type', 'theta_dependence': True, 'U': U, 'V':V, 'W':W, 'A':A, 'eta_h':eta_h, 'eta_l':eta_l}
 
-        struct = crystal('cif', filename=save_path)
-        xrd1 = XRD(struct, wavelength, max2theta) 
-        xrd1.get_profile(xrd1.theta2, xrd1.xrd_intensity, N, **profile)
-        plot = plot_pxrd(xrd1)
+        struct = read(save_path, format='cif')
+        xrd = XRD(struct, wavelength=wavelength, max2theta=max2theta) 
+        xrd.get_profile(xrd.theta2, xrd.xrd_intensity, N, **profile)
+        plot = xrd.plotly_pxrd()
 
         return render_template('index.html', title='Calculator', form=form, plot=plot)
     return render_template('index.html', title='Calculator', form=form)
