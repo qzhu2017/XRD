fb85cf479ba0c199aae08ef015139d484ff044ee
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2020-02-15 15:32:56 -0800

    Add session logic to address re-upload issue

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import render_template, flash
+from flask import render_template, flash, session
 from app import app
 from app.forms import CalcForm
 from werkzeug.utils import secure_filename
@@ -11,24 +11,65 @@ from ase.io import read
 def index():
     form = CalcForm()
     if form.validate_on_submit():
-        # Save uploaded files to disk
-        f = form.upload.data
-        save_path = os.path.join(app.instance_path, 'uploads', secure_filename(f.filename))
-        f.save(save_path)
-        # ext = os.path.splitext(save_path)[1] # retrieve file extension
+        # Brand new session
+        if not session.get("SAVEPATH"):
+            if form.upload.data:
+                save_upload(form)
+                return render_template('index.html', 
+                    title='Calculator',
+                    form=form,
+                    plot=plot(form))
+            else:
+                flash('Please upload a .CIF or -POSCAR file!',
+                    'danger')
+                return render_template('index.html',
+                    title='Calculator',
+                    form=form)
+        # Existing upload
+        else:
+            if form.upload.data: # newer upload
+                save_upload(form) 
+            return render_template('index.html', 
+                title='Calculator',
+                form=form,
+                plot=plot(form))
+    return render_template('index.html',
+        title='Calculator',
+        form=form)
 
-        # Retrieve form data
-        wavelength = form.wavelength.data
-        max2theta = form.theta.data
-        min2theta = 0 # form.theta.data1
-        N = 10000
-        # Alert w/ submission data
-        flash('XRD calculated for {}, λ={} Å, 2θ={}°'.format(f.filename, wavelength, max2theta))
+def save_upload(form):
+    """
+    Convenience function to save uploads and update session
+    """
+    # Save uploaded file to disk
+    f = form.upload.data
+    savepath = os.path.join(app.instance_path, 
+        'uploads', secure_filename(f.filename))
+    f.save(savepath)
+    # Update session data
+    session["FILENAME"] = f.filename
+    session["SAVEPATH"] = savepath
+        
+def plot(form):
+    """
+    Convenience function to process and return PXRD plotly
+    """
+    # Retrieve form data
+    wavelength = form.wavelength.data
+    max2theta = form.theta.data
+    min2theta = 0 # form.theta.data1
+    N = 10000
 
-        struct = read(save_path)
-        xrd = XRD(struct, wavelength=wavelength, thetas=[0, max2theta]) 
-        xrd.get_profile(res=0.01)
-        plot = xrd.plotly_pxrd()
+    struct = read(session.get("SAVEPATH"))
+    xrd = XRD(struct,
+        wavelength=wavelength,
+        thetas=[min2theta, max2theta]) 
+    xrd.get_profile(res=0.01)
 
-        return render_template('index.html', title='Calculator', form=form, plot=plot)
-    return render_template('index.html', title='Calculator', form=form)
+    flash('XRD for {} plotted below.'.format(
+        session.get("FILENAME")), 'success')
+
+    return xrd.plotly_pxrd()
+
+# retrieve file extension
+# ext = os.path.splitext(savepath)[1]
