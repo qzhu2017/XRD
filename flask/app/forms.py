f279b04839bfc21cd9e4b8aa1de02983811a0e74
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2020-02-16 16:20:09 -0800

    Add param fields & plot w/ profiles

--- flask/app/forms.py
+++ flask/app/forms.py
@@ -2,13 +2,14 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import FloatField, SelectField, SubmitField
 from wtforms.validators import DataRequired, NumberRange, ValidationError
+# from wtforms import StringField, FieldList, FormField
 
 class CalcForm(FlaskForm):
     upload = FileField(
         label='Input file (CIF, POSCAR, etc.)',
         description='See <a href="https://wiki.fysik.dtu.dk/ase/ase/io/io.html?highlight=formats#file-input-and-output" target="_blank">table</a> for a list of readable formats')
     wavelength = FloatField(
-        label='&lambda; (&#8491;)',
+        label='<i>&lambda;</i> (&#8491;)',
         validators=[
             DataRequired(),
             NumberRange(
@@ -18,7 +19,7 @@ class CalcForm(FlaskForm):
         description='X-ray wavelength in angstroms',
         default=1.54056)
     min2theta = FloatField(
-        label='2&theta;<sub>min</sub> (&deg;)',
+        label='2<i>&theta;</i><sub>min</sub> (&deg;)',
         validators=[
             NumberRange(
                 min=0,
@@ -27,7 +28,7 @@ class CalcForm(FlaskForm):
         description='Minimum diffraction angle in degrees',
         default=0)
     max2theta = FloatField(
-        label='2&theta;<sub>max</sub> (&deg;)',
+        label='2<i>&theta;</i><sub>max</sub> (&deg;)',
         validators=[
             DataRequired(),
             NumberRange(
@@ -45,12 +46,101 @@ class CalcForm(FlaskForm):
                 max=1,
                 message='Must be between %(min)s° and %(max)s°')
             ],
-        description='Plot resolution in degrees',
+        description='Resolution in degrees',
         default=0.01)
-    profile = SelectField(
-        label='Profile',
-        choices=[('gaussian', 'Gaussian'), ('lorentzian', 'Lorentzian'), ('split-type', 'Split-type')],
-        description='Profiling function applied to simulated XRD pattern')
+    method = SelectField(
+        label='Profiling Function',
+        choices=[('gaussian', 'Gaussian'),
+            ('lorentzian', 'Lorentzian'),
+            ('pseudo_voigt', 'Pseudo-Voigt')],
+        description='Applied to simulated XRD pattern')
+    fwhm = FloatField(
+        label='FWHM',
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=1e-3,
+                max=1,
+                message='Must be between %(min)s and %(max)s')
+            ],
+        description='Full width at half maximum',
+        default=0.02)
+    u = FloatField(
+        label='<i>U</i>',
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=1e-3,
+                max=1,
+                message='Must be between %(min)s and %(max)s')
+            ],
+        description='Pseudo-Voigt parameter',
+        default=5.776410E-03)
+    v = FloatField(
+        label='<i>V</i>',
+        validators=[
+            DataRequired(),
+            # NumberRange(
+            #     min=1e-3,
+            #     max=1,
+            #     message='Must be between %(min)s and %(max)s')
+            ],
+        description='Pseudo-Voigt parameter',
+        default=-1.673830E-03)
+    w = FloatField(
+        label='<i>W</i>',
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=1e-3,
+                max=1,
+                message='Must be between %(min)s and %(max)s')
+            ],
+        description='Pseudo-Voigt parameter',
+        default=5.668770E-03)
+    a = FloatField(
+        label='<i>A</i>',
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=0.1,
+                max=5,
+                message='Must be between %(min)s and %(max)s')
+            ],
+        description='Pseudo-Voigt parameter',
+        default=1.03944)
+    eta_h = FloatField(
+        label='<i>&eta;</i><sub>h</sub>',
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=0.1,
+                max=1,
+                message='Must be between %(min)s and %(max)s')
+            ],
+        description='Pseudo-Voigt parameter',
+        default=0.504656)
+    eta_l = FloatField(
+        label='<i>&eta;</i><sub>l</sub>',
+        validators=[
+            DataRequired(),
+            NumberRange(
+                min=0.1,
+                max=1,
+                message='Must be between %(min)s and %(max)s')
+            ],
+        description='Pseudo-Voigt parameter',
+        default=0.611844)
     submit = SubmitField('Visualize')
+
+# Create separate classes for each profiling branch
+# w/ member parameters (see "Field Enclosures")
+# class IMForm(FlaskForm):
+#     protocol = SelectField(choices=[('aim', 'AIM'), ('msn', 'MSN')])
+#     username = StringField()
+
+# class ContactForm(FlaskForm):
+#     first_name  = StringField()
+#     last_name   = StringField()
+#     im_accounts = FieldList(FormField(IMForm))
     
-# Create separate classes for each profiling branch w/ member parameters (see "Field Enclosures")
--- flask/app/routes.py
+++ flask/app/routes.py
@@ -14,8 +14,8 @@ def index():
         if form.upload.data:
             process_upload(form)
         if not session.get("SAVEPATH"): # new session
-            flash('ERROR: Please upload an input file.', 
-                'danger')
+            flash(Markup('<b>ERROR</b>: Please upload an\
+                input file.'), 'danger')
             return render_template('index.html',
                 title='Calculator',
                 form=form)
@@ -47,51 +47,73 @@ def process_upload(form):
         # Update session keys
         session["FILENAME"] = f.filename
         session["SAVEPATH"] = savepath
-        flash('SUCCESS: {} processed.'.format(
-            session.get("FILENAME")), 'success')
+        flash(Markup('<b>{}</b> successfully\
+            processed.').format(session.get("FILENAME")), 
+            'success')
     except:
-        flash('ERROR: Unable to read {}.\
-            Please try again or a different file.'.format(
-            f.filename),
-            'danger')
+        flash(Markup('<b>ERROR</b>: Unable to read\
+            <b>{}</b>. Please try again or a different\
+            file.').format(f.filename), 'danger')
 
 def process_form(form):
     """
     Advanced form validation and session update.
     """
     # Retrieve form data
-    wavelength = form.wavelength.data
     max2theta = form.max2theta.data
     min2theta = form.min2theta.data
-    res = form.res.data
-    profile = form.profile.data
 
     # Advanced form-level validation
     if min2theta > max2theta:
         min2theta = 0 # use default
         flash(Markup('WARNING: 2&theta;<sub>min</sub>\
-            greater than\
+            <i>greater</i> than\
             2&theta;<sub>max</sub>&mdash;defaulting\
             2&theta;<sub>min</sub> to 0&deg;.'), 'warning')
 
     # Update session keys
-    session["WAVELENGTH"] = wavelength
+    session["WAVELENGTH"] = form.wavelength.data
     session["MIN2THETA"] = min2theta
     session["MAX2THETA"] = max2theta
-    session["RES"] = res
-    session["PROFILE"] = profile
+    session["RES"] = form.res.data
+    session["METHOD"] = form.method.data
+    session["FWHM"] = form.fwhm.data
+    session["U"] = form.u.data
+    session["V"] = form.v.data
+    session["W"] = form.w.data
+    session["A"] = form.a.data
+    session["eta_h"] = form.eta_h.data
+    session["eta_l"] = form.eta_l.data
         
 def plot(form):
     """
     Process and return PXRD plotly.
     """
+    method = session.get("METHOD")
+    if method == 'gaussian' or method == 'lorentzian':
+        kwargs = {
+                    'FWHM': session.get("FWHM")
+                }
+    elif method == 'pseudo_voigt':
+        kwargs = {
+                    'U': session.get("U"), 
+                    'V': session.get("V"),
+                    'W': session.get("W"),
+                    'A': session.get("A"),
+                    'eta_h': session.get("eta_h"),
+                    'eta_l': session.get("eta_l"),
+                }
+    print("***DEBUG OUT:", kwargs)
+
     struct = read(session.get("SAVEPATH"))
     xrd = XRD(struct,
         wavelength=session.get("WAVELENGTH"),
         thetas=[session.get("MIN2THETA"),
             session.get("MAX2THETA")]) 
-    xrd.get_profile(res=session.get("RES"))
-    flash('Showing {} with {} profile.'.format(
+    xrd.get_profile(method=method,
+        res=session.get("RES"))
+    flash(Markup('Showing <b>{}</b> with <i>{}</i>\
+        profiling.').format(
             session.get("FILENAME"),
-            session.get("PROFILE")), 'info')
+            method), 'info')
     return xrd.plotly_pxrd()
