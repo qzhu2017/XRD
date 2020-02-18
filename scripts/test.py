87d591feeeca204cb42f297d612845ce8262b99a
Dean <sayred1@unlv.nevada.edu>
2020-02-18 11:27:24 -0800

    update profiling demo

--- scripts/test.py
+++
@@ -1,7 +0,0 @@
-# a test script to calculate the similarity between two structures 
-
-from scripts.compare import Compare
-
-path = './dataset/'
-
-Compare(path+'A-1-POSCAR', 'POSCAR', path+'A-1-POSCAR', 'POSCAR')
