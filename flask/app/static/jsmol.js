a02536f2dcf74fd28074cc1bec539212eae1b480
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2020-03-17 19:07:55 -0700

    Display 2 Jmol objs and refactor JS

--- flask/app/static/jsmol.js
+++ flask/app/static/jsmol.js
@@ -1,25 +1,39 @@
-Jmol._isAsync = false;
-
-var jmolApplet0;
+var jmol;
 var Info = {
-  width: 550,
-  height: 400,
-  debug: false,
   color: "0xFFFFFF",
-  addSelectionOptions: false,
+  height: 400,
+  width: 550,
   use: "HTML5",
+  allowJavaScript: false,
   j2sPath: "/static/jsmol/j2s",
-  readyFunction: jmol_isReady,
+  script: "set displaycellparameters false; load struct/cif { 1 1 1 };",
+  addSelectionOptions: false,
   disableJ2SLoadMonitor: true,
   disableInitialConsole: true,
-  allowJavaScript: false
+  debug: false,
+};
+var jmol2;
+var Info2 = {
+  color: "0xFFFFFF",
+  height: 400,
+  width: 550,
+  use: "HTML5",
+  allowJavaScript: false,
+  j2sPath: "/static/jsmol/j2s",
+  script: "set displaycellparameters false; load struct2/cif { 1 1 1 };",
+  addSelectionOptions: false,
+  disableJ2SLoadMonitor: true,
+  disableInitialConsole: true,
+  debug: false,
 };
 
-function repeatCell(n1, n2, n3) {
+function repeatCell(n1, n2, n3, num) {
   var s = '{ ' + n1.toString() + ' ' + n2.toString() + ' ' + n3.toString() + ' };';
-  Jmol.script(jmolApplet0, 'load "" ' + s);
+  if (num) Jmol.script(jmol2, 'load "" ' + s);
+  else Jmol.script(jmol, 'load "" ' + s);
 }
 
 $(document).ready(function() {
-  $("#appdiv").html(Jmol.getAppletHtml("jmolApplet0", Info));
+  $("#appdiv").html(Jmol.getAppletHtml("jmol", Info));
+  $("#appdiv2").html(Jmol.getAppletHtml("jmol2", Info2));
 })
