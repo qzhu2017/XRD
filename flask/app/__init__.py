d9f9e406a5f1161ab953df6bf9c7eb54fa7ad402
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2020-04-03 20:14:11 -0700

    Configured sending errors by email

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,7 +1,10 @@
+
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
 from flask_uploads import patch_request_class
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,3 +12,19 @@ bootstrap = Bootstrap(app)
 patch_request_class(app, size=16777216)
 
 from app import routes, errors
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='VXRD Error Traceback',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
--- flask/config.py
+++ flask/config.py
@@ -2,4 +2,10 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'tR4rcW_A2JrGmZWWj-UtLA'
-    
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = 'vxrd.info'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['vxrd.info@gmail.com']
